73bb67e80e36795e7a8c5ea2b9c4e203ceea0e84
jonoman55 <jonoman55@yahoo.com>
2021-06-03 20:29:19 -0400

    separated the LogFileHelper class from LogFile.cs

--- GTAWS_v2/Logging/LogFile.cs
+++ GTAWS_v2/Logging/LogFile.cs
@@ -3,7 +3,6 @@ using GTAWS_v2.Interfaces;
 using GTAWS_v2.Tools;
 using System;
 using System.IO;
-using System.Linq;
 
 namespace GTAWS_v2.Logging
 {
@@ -128,58 +127,5 @@ namespace GTAWS_v2.Logging
         /// <param name="file"></param>
         /// <returns></returns>
         public bool CheckFileSize(string file) => LogFileTools.CheckFileSize(file); // File Size limit = 10MB
-    }
-
-    public class LogFileHelper
-    {
-        /// <summary>
-        /// Used for adding an entry to the log file without having to create a new LogFile object.
-        /// </summary>
-        /// <param name="data"></param>
-        /// <param name="level"></param>
-        public static void AddEntry(string data, LogLevel level)
-        {
-            AddEntries(new LogEntry() { Data = data, Level = level });
-        }
-
-        /// <summary>
-        /// Used for logging multiple Log Entries to the log file.
-        /// </summary>
-        public static void AddEntries(params LogEntry[] entries)
-        {
-            foreach ((LogEntry e, string msg) in from e in entries
-                                                 let data = e.ToString()
-                                                 select (e, data)) // Extract The Log Entry and message from the Array
-            {
-                if (e == null || string.IsNullOrEmpty(msg)) // Check if the data is valid
-                {
-                    continue;
-                }
-                else
-                {
-                    new LogFile().AddEntry(e); // Add the Log Entry to the log file.
-                }
-            }
-        }
-
-        public static void AddWarningEntry(string msg)
-        {
-            AddEntry(msg, LogLevel.Warning);
-        }
-
-        public static void AddErrorEntry(string msg)
-        {
-            AddEntry(msg, LogLevel.Error);
-        }
-
-        public static void AddInfoEntry(string msg)
-        {
-            AddEntry(msg, LogLevel.Info);
-        }
-
-        public static void AddDebugEntry(string msg)
-        {
-            AddEntry(msg, LogLevel.Debug);
-        }
     }
 }
---
+++ GTAWS_v2/Logging/LogFileHelper.cs
@@ -0,0 +1,58 @@
+﻿using GTAWS_v2.Enums;
+using System.Linq;
+
+namespace GTAWS_v2.Logging
+{
+    public class LogFileHelper
+    {
+        /// <summary>
+        /// Used for adding an entry to the log file without having to create a new LogFile object.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="level"></param>
+        public static void AddEntry(string data, LogLevel level)
+        {
+            AddEntries(new LogEntry() { Data = data, Level = level });
+        }
+
+        /// <summary>
+        /// Used for logging multiple Log Entries to the log file.
+        /// </summary>
+        public static void AddEntries(params LogEntry[] entries)
+        {
+            foreach ((LogEntry e, string msg) in from e in entries
+                                                 let data = e.ToString()
+                                                 select (e, data)) // Extract The Log Entry and message from the Array
+            {
+                if (e == null || string.IsNullOrEmpty(msg)) // Check if the data is valid
+                {
+                    continue;
+                }
+                else
+                {
+                    new LogFile().AddEntry(e); // Add the Log Entry to the log file.
+                }
+            }
+        }
+
+        public static void AddWarningEntry(string msg)
+        {
+            AddEntry(msg, LogLevel.Warning);
+        }
+
+        public static void AddErrorEntry(string msg)
+        {
+            AddEntry(msg, LogLevel.Error);
+        }
+
+        public static void AddInfoEntry(string msg)
+        {
+            AddEntry(msg, LogLevel.Info);
+        }
+
+        public static void AddDebugEntry(string msg)
+        {
+            AddEntry(msg, LogLevel.Debug);
+        }
+    }
+}
